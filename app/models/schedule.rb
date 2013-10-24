dfe40745b68f22ba6366161aaff94cdc26eba105
Kevin Lester <kevin@e-kevin.com>
2013-10-24 17:22:03 -0400

    added beginnings of ice_cube logic and tests

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,4 +1,32 @@
+require 'ice_cube'
+
 class Schedule < ActiveRecord::Base
   attr_accessible :monthly_day, :once_date, :kind, :weekly_day, :yearly_date, :yearly_day, :yearly_month
   belongs_to :tasks
+
+  def ice_cube
+    send("ic_#{kind}")
+  end
+
+  def ic_none
+    nil
+  end
+  
+  def ic_yearly
+    add_rule(IceCube::Rule.yearly.month_of_year(yearly_date.month).day_of_month(yearly_date.day))
+  end
+
+  def ic_monthly
+    add_rule(IceCube::Rule.monthly.day_of_month(monthly_day))
+  end
+
+  def ic_once
+    IceCube::Schedule.new(once_date) 
+  end
+
+  def add_rule(rule)
+    ic = IceCube::Schedule.new
+    ic.add_recurrence_rule rule
+    ic
+  end
 end
--- spec/models/schedule_spec.rb
+++ spec/models/schedule_spec.rb
@@ -1,5 +1,36 @@
 require 'spec_helper'
 
 describe Schedule do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+
+  describe "do once" do
+    let(:s) { Schedule.new(kind: :once, once_date: Date.today) }
+
+    it { s.kind.should be :once }
+    it { s.ice_cube.occurs_on?(Date.today).should be true }
+    it { s.ice_cube.occurs_on?(Date.today + 1.year).should be false }
+    it { s.ice_cube.occurs_on?(Date.yesterday).should be false }
+  end
+
+  describe "on yearly" do
+    let(:s) { Schedule.new(kind: :yearly, yearly_date: Date.today) }
+
+    it { s.ice_cube.occurs_on?(Date.today + 1.year).should be true }
+    it { s.ice_cube.occurs_on?(Date.today + 1.year + 1.day).should be false }
+  end
+
+  describe "do monthly" do
+    let(:d) { Date.new(2013, 10, 24)}
+    let(:s) { Schedule.new(kind: :monthly, monthly_day: d.day) }
+
+    it { s.ice_cube.occurs_on?(d).should be true }
+    it { s.ice_cube.occurs_on?(d + 1.month).should be true }
+    it { s.ice_cube.occurs_on?(d + 1.month + 1.day).should be false }
+  end
+
+  describe "for none" do
+    let(:s) { Schedule.new(kind: :none) }
+
+    it { s.ice_cube.should be nil }
+  end
 end
