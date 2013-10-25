c625564bdae16e62ce5ca869ffe6954f8857ae43
Kevin Lester <kevin@e-kevin.com>
2013-10-25 15:10:15 -0400

    added occurs_on? and exists? to mask use of ice_cube from user

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -4,6 +4,14 @@ class Schedule < ActiveRecord::Base
   attr_accessible :monthly_day, :once_date, :kind, :weekly_day, :yearly_date, :yearly_day, :yearly_month
   belongs_to :tasks
 
+  def occurs_on?(date)
+    ice_cube.occurs_on?(date)
+  end
+
+  def exists?
+    not ice_cube.nil?
+  end
+
   def ice_cube
     send("ic_#{kind}")
   end
--- spec/models/schedule_spec.rb
+++ spec/models/schedule_spec.rb
@@ -6,17 +6,16 @@ describe Schedule do
   describe "do once" do
     let(:s) { Schedule.new(kind: :once, once_date: Date.today) }
 
-    it { s.kind.should be :once }
-    it { s.ice_cube.occurs_on?(Date.today).should be true }
-    it { s.ice_cube.occurs_on?(Date.today + 1.year).should be false }
-    it { s.ice_cube.occurs_on?(Date.yesterday).should be false }
+    it { s.should be_occurs_on(Date.today) }
+    it { s.should_not be_occurs_on(Date.today + 1.year) }
+    it { s.should_not be_occurs_on(Date.yesterday) }
   end
 
   describe "on yearly" do
     let(:s) { Schedule.new(kind: :yearly, yearly_date: Date.today) }
 
-    it { s.ice_cube.occurs_on?(Date.today + 1.year).should be true }
-    it { s.ice_cube.occurs_on?(Date.today + 1.year + 1.day).should be false }
+    it { s.should be_occurs_on(Date.today + 1.year) }
+    it { s.should_not be_occurs_on(Date.today + 1.year + 1.day) }
   end
 
   describe "do monthly" do
@@ -24,13 +23,13 @@ describe Schedule do
     let(:s) { Schedule.new(kind: :monthly, monthly_day: d.day) }
 
     it "specific day" do 
-      s.ice_cube.occurs_on?(d).should be true 
+      s.should be_occurs_on(d)
     end 
     it "specific day + 1 month" do 
-      s.ice_cube.occurs_on?(d + 1.month).should be true 
+      s.should be_occurs_on(d + 1.month)
     end 
     it "specific day + a month and a day" do 
-      s.ice_cube.occurs_on?(d + 1.month + 1.day).should be false 
+      s.should_not be_occurs_on(d + 1.month + 1.day)
     end
   end
 
@@ -38,21 +37,22 @@ describe Schedule do
     let(:d) { Date.new((Date.today + 1.year).year, Date.today.month, 1) }
     let(:s) { Schedule.new(kind: :first_day_month) }
 
-    it { s.ice_cube.occurs_on?(d).should be true } 
-    it { s.ice_cube.occurs_on?(d + 1.day).should be false }
+    it { s.should be_occurs_on(d) } 
+    it { s.should_not be_occurs_on(d + 1.day) }
   end
 
   describe "do last day of month" do
     let(:d) { Date.new((Date.today + 1.year).year, Date.today.month, 1) - 1.day }
     let(:s) { Schedule.new(kind: :last_day_month) }
 
-    it { s.ice_cube.occurs_on?(d).should be true } 
-    it { s.ice_cube.occurs_on?(d + 1.day).should be false }
+    it { s.should be_occurs_on(d) } 
+    it { s.should_not be_occurs_on(d + 1.day) }
   end
 
   describe "for none" do
     let(:s) { Schedule.new(kind: :none) }
 
-    it { s.ice_cube.should be nil }
+    # it { s.exists?.should be false } 
+    it { s.should_not be_exists } 
   end
 end
