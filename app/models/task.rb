52746163224cd4d3bfacfde851c7313884314772
Kevin Lester <kevin@e-kevin.com>
2013-12-06 15:12:25 -0500

    filter was dropping tasks that did not have a status

--- app/models/task.rb
+++ app/models/task.rb
@@ -11,9 +11,13 @@ class Task < ActiveRecord::Base
 
   def self.filters(args)
     set = Task.select('DISTINCT tasks.*')
-    set = set.joins(:company).joins(:category).joins(:statuses)
-    set = set.where("category_id = ?", args[:category_id]) if args.has_key?(:category_id) and !args[:category_id].empty?
-    set = set.where("company_id = ?", args[:company_id]) if args.has_key?(:company_id) and !args[:company_id].empty?
+    set = set.joins(:company).joins(:category).includes(:statuses)
+    if args.has_key?(:category_id) and !args[:category_id].empty?
+      set = set.where("category_id = ?", args[:category_id]) 
+    end
+    if args.has_key?(:company_id) and !args[:company_id].empty?
+      set = set.where("company_id = ?", args[:company_id]) 
+    end
     if args.has_key?(:user_id) and !args[:user_id].empty?
       set = set.where("tasks.user_id = ? or statuses.user_id=?", args[:user_id], args[:user_id])
     end
