05ba01250d56e39bcff558b73928d59bd4be930f
Kevin Lester <kevin@e-kevin.com>
2014-03-17 18:27:41 -0400

    still working on revamping scheduler

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,9 +1,31 @@
-$(function(){ 
-  $(".schedule_type_select").change(function() { 
+jQuery.fn.loadScheduleTypeForm = function() {
+  this.find(".schedule_type_select").change(function() { 
     var key = "#" + $(this).attr('id') + " option:selected";
     var kind = $(key).val();
     var se = $(this).closest('div');
     se.find('div.container').hide();
     se.find('#schedule_' + kind).show();
   });
+}
+
+jQuery.fn.defaultDatePicker = function  () {
+  this.datepicker({ 
+    dateFormat: "yy-mm-dd",
+    changeMonth: true, 
+    changeYear: true, 
+  });
+  this.attr('readonly', true);
+}
+
+jQuery.fn.refreshSchedulerEvents = function() {
+  this.loadScheduleTypeForm();
+  this.find('.scheduler_once_date').defaultDatePicker();
+  this.find('.scheduler_yearly_date').defaultDatePicker();
+  this.find('.scheduler_weekly_date').defaultDatePicker();
+}
+
+$(function() { 
+  $(".task-scheduler-div").refreshSchedulerEvents();
 });
+
+
